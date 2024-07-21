ce9bbcad71bf478db0bbf092083a749dedd4cd3c
Matthew Thomas <mthomas@ftc.gov>
2024-07-21 11:16:21 -0400

    updates, add uncss, smaller images

--- scripts/render-rsvp.js
+++ scripts/render-rsvp.js
@@ -4,25 +4,32 @@ const sh = require("shelljs");
 
 module.exports = function renderRSVP() {
   const sourcePath = upath.resolve(upath.dirname(__filename), "../src/rsvp/");
-  const destPath = upath.resolve(upath.dirname(__filename), "../dist/en/rsvp/");
+  const destPathEn = upath.resolve(upath.dirname(__filename), "../dist/en/rsvp/");
+  const destPathPt = upath.resolve(upath.dirname(__filename), "../dist/pt/rsvp/");
 
   // Create the rsvp directory in the destination if it doesn't exist
-  if (!sh.test("-e", destPath)) {
-    sh.mkdir("-p", destPath);
+  if (!sh.test("-e", destPathEn)) {
+    sh.mkdir("-p", destPathEn);
+  }
+  if (!sh.test("-e", destPathPt)) {
+    sh.mkdir("-p", destPathPt);
   }
 
   // Recursively copy all files from the source to the destination unless they have the .pug extension
   sh.ls("-RA", sourcePath).forEach((file) => {
     let sourceFile = upath.resolve(sourcePath, file);
-    let destFile = upath.resolve(destPath, file);
+    let destFileEn = upath.resolve(destPathEn, file);
+    let destFilePt = upath.resolve(destPathPt, file);
 
     // Copy all non-pug files and create directories if they don't exist
-    if (sh.test("-d", sourceFile) && !sh.test("-e", destFile)) {
-      console.log(`### INFO: Creating directory ${destFile}`);
-      sh.mkdir(destFile);
+    if (sh.test("-d", sourceFile)) {
+      console.log(`### INFO: Creating directories ${destFileEn} and ${destFilePt}`);
+      sh.mkdir("-p", destFileEn);
+      sh.mkdir("-p", destFilePt);
     } else if (sh.test("-f", sourceFile) && !file.endsWith(".pug")) {
-      console.log(`### INFO: Copying ${sourceFile} to ${destFile}`);
-      sh.cp(sourceFile, destFile);
+      console.log(`### INFO: Copying ${sourceFile} to ${destFileEn} and ${destFilePt}`);
+      sh.cp(sourceFile, destFileEn);
+      sh.cp(sourceFile, destFilePt);
     }
   });
 };
