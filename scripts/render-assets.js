2492fe14b011d38a78b2884ccd947ef2d05f18b4
Matthew Thomas <mthomas@ftc.gov>
2024-07-22 16:21:58 -0400

    Move favicon to root folder

--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -1,11 +1,20 @@
-'use strict';
-const fs = require('fs');
-const upath = require('upath');
-const sh = require('shelljs');
+"use strict";
+const upath = require("upath");
+const sh = require("shelljs");
 
 module.exports = function renderAssets() {
-    const sourcePath = upath.resolve(upath.dirname(__filename), '../src/assets');
-    const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
-    
-    sh.cp('-R', sourcePath, destPath)
+  const sourcePath = upath.resolve(upath.dirname(__filename), "../src/assets");
+  const destPath = upath.resolve(upath.dirname(__filename), "../dist/.");
+
+  sh.cp("-R", sourcePath, destPath);
+
+  // Move the favicon.ico file to the root of the dist directory, if it exists
+  const faviconPath = upath.resolve(
+    upath.dirname(__filename),
+    "../dist/assets/favicon.ico"
+  );
+
+  if (sh.test("-e", faviconPath)) {
+    sh.mv(faviconPath, destPath);
+  }
 };
