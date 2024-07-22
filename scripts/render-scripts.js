1b0d47630f61038b6a94b6b23b89f24951b7b060
Matthew Thomas <mthomas@ftc.gov>
2024-07-22 09:41:07 -0400

    js minify + consolidation + hotel update

--- scripts/render-scripts.js
+++ scripts/render-scripts.js
@@ -1,26 +1,33 @@
-'use strict';
-const fs = require('fs');
-const packageJSON = require('../package.json');
-const upath = require('upath');
-const sh = require('shelljs');
+"use strict";
+const fs = require("fs");
+const packageJSON = require("../package.json");
+const upath = require("upath");
+const sh = require("shelljs");
+const UglifyJS = require("uglify-js");
 
 module.exports = function renderScripts() {
+  // Create dist/js directory if it doesn't exist
+  const destPath = upath.resolve(upath.dirname(__filename), "../dist/js");
+  if (!sh.test("-d", destPath)) {
+    sh.mkdir("-p", destPath);
+  }
 
-    const sourcePath = upath.resolve(upath.dirname(__filename), '../src/js');
-    const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
-    
-    sh.cp('-R', sourcePath, destPath)
+  const sourcePathScriptsJS = upath.resolve(
+    upath.dirname(__filename),
+    "../src/js/scripts.js"
+  );
+  const destPathScriptsJS = upath.resolve(
+    upath.dirname(__filename),
+    "../dist/js/scripts.js"
+  );
 
-    const sourcePathScriptsJS = upath.resolve(upath.dirname(__filename), '../src/js/scripts.js');
-    const destPathScriptsJS = upath.resolve(upath.dirname(__filename), '../dist/js/scripts.js');
-    
-    const copyright = `/*!
-* Start Bootstrap - ${packageJSON.title} v${packageJSON.version} (${packageJSON.homepage})
-* Copyright 2013-${new Date().getFullYear()} ${packageJSON.author}
-* Licensed under ${packageJSON.license} (https://github.com/StartBootstrap/${packageJSON.name}/blob/master/LICENSE)
-*/
-`
-    const scriptsJS = fs.readFileSync(sourcePathScriptsJS);
-    
-    fs.writeFileSync(destPathScriptsJS, copyright + scriptsJS);
+  // Read bootstrap js file
+  const bootstrapJS = fs.readFileSync(require.resolve("bootstrap"), "utf8");
+
+  const scriptsJS = fs.readFileSync(sourcePathScriptsJS, "utf8");
+
+  // Uglify scripts.js and write to dist/js directory
+  const minifiedJS = UglifyJS.minify(scriptsJS + bootstrapJS);
+
+  fs.writeFileSync(destPathScriptsJS, minifiedJS.code);
 };
