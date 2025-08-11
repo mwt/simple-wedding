cabb98d9e35de8c53aac6c8b2b56de2eec22d7ae
Matthew Thomas <matthew@mwt.me>
2025-08-11 12:26:55 -0400

    lightbox on album

--- scripts/render-scripts.js
+++ scripts/render-scripts.js
@@ -3,14 +3,38 @@ const fs = require("fs");
 const upath = require("upath");
 const UglifyJS = require("uglify-js");
 
-module.exports = function renderScripts() {
-  const sourcePathScriptsJS = upath.resolve(
+module.exports = function renderScripts(includedScripts = ["simple-lightbox", "homepage", "album"]) {
+  // Get path to simple-lightbox
+  const sourcePathSimpleLightbox = require.resolve(
+    "simple-lightbox/dist/simpleLightbox.js"
+  );
+
+  // Get path to homepage script
+  const sourcePathHomepageJS = upath.resolve(
     upath.dirname(__filename),
-    "../src/js/scripts.js"
+    "../src/js/homepage.js"
+  );
+
+  // Get path to album script
+  const sourcePathAlbumJS = upath.resolve(
+    upath.dirname(__filename),
+    "../src/js/album.js"
   );
 
   // Read scripts.js file
-  const scriptsJS = fs.readFileSync(sourcePathScriptsJS, "utf8");
+  const scriptsJS = {};
+
+  if (includedScripts.includes("simple-lightbox")) {
+    scriptsJS["simple-lightbox.js"] = fs.readFileSync(sourcePathSimpleLightbox, "utf8");
+  }
+
+  if (includedScripts.includes("homepage")) {
+    scriptsJS["homepage.js"] = fs.readFileSync(sourcePathHomepageJS, "utf8");
+  }
+
+  if (includedScripts.includes("album")) {
+    scriptsJS["album.js"] = fs.readFileSync(sourcePathAlbumJS, "utf8");
+  }
 
   // Uglify scripts.js and write to dist/js directory
   const minifiedJS = UglifyJS.minify(scriptsJS);
---
+++ src/js/album.js
@@ -0,0 +1,37 @@
+//
+// Scripts
+//
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  "use strict";
+
+  /*=============================================================================
+   Simple Lightbox Section
+  =============================================================================*/
+
+  // Initialize the image gallery lightbox
+  new SimpleLightbox({ elements: ".image-gallery a" });
+
+  /*=============================================================================
+   Mobile Navbar Section
+  =============================================================================*/
+
+  const mobileNavbar = document.getElementById("navbarToggle");
+
+  mobileNavbar.addEventListener("click", (e) => {
+    // Toggle the aria-expanded attribute
+    e.currentTarget.setAttribute(
+      "aria-expanded",
+      e.currentTarget.getAttribute("aria-expanded") === "true"
+        ? "false"
+        : "true"
+    );
+
+    // Toggle the collapsed class (doesn't do anything, but bootstrap does it)
+    e.currentTarget.classList.toggle("collapsed");
+
+    // Toggle the show class
+    const navbar = document.getElementById("navbarResponsive");
+    navbar.classList.toggle("show");
+  });
+});
